44cae1b23e3c81841be528136ac84e8bab2caf4c
Igor_chu <igorek_churilow@mail.ru>
2023-08-10 21:06:45 +0400

    add form with input title, template admin.category.create

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.layouts.main')
+
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+
+            <div class="container-fluid">
+
+                <div class="row mb-2">
+
+                    <div class="col-sm-6">
+
+                        <h1 class="m-0">{{__('Добавление категории')}}</h1>
+
+                    </div><!-- /.col -->
+
+                    <div class="col-sm-6">
+
+                        <ol class="breadcrumb float-sm-right">
+
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+
+                        </ol>
+
+                    </div><!-- /.col -->
+
+                </div><!-- /.row -->
+
+            </div><!-- /.container-fluid -->
+
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+
+                    <div class="col-12">
+
+                        <form action="#" class="w-25">
+
+                            <div class="form-group">
+
+                                <label>Заголовок</label>
+
+                                <input type="text" class="form-control mb-3" placeholder="Введите заголовок">
+
+                                <input type="submit" class="btn btn-primary" value="Добавить">
+
+                            </div>
+
+                        </form>
+
+
+                    </div>
+
+                </div>
+                <!-- /.row -->
+
+            </div><!-- /.container-fluid -->
+
+        </section>
+        <!-- /.content -->
+
+    </div>
+
+@endsection
